7680be1f2e4814ab239f13315925c1c6e588ffea
Malcolm Jones <bossjones@theblacktonystark.com>
2022-06-09 21:16:43 -0400

    Feature: dsm7, cmds

--- scripts/start-snmp-exporter.sh
+++ scripts/start-snmp-exporter.sh
@@ -4,14 +4,17 @@ set -x
 
 _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+sudo docker rm snmp_exporter || true
+
 sudo docker run \
 --restart=always \
 --name=snmp_exporter \
 --net=host \
 --pid=host \
---user 65534:65534 \
 --volume ${_DIR}/../snmp_exporter/:/etc/snmp_exporter/ \
 --volume ${_DIR}/../snmp-synology/snmp.yml:/etc/snmp_exporter/snmp.yml:ro \
 prom/snmp-exporter:v0.20.0 \
 --config.file=/etc/snmp_exporter/snmp.yml \
 --log.level=debug
+
+# --user 65534:65534 \
